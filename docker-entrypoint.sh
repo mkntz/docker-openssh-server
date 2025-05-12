394c677214861ac9dc4c42e6fb72d1d26d689479
Makan Taghizadeh <me@mkntz.com>
2025-05-12 18:58:32 +0200

    feat: add support for login with ssh key for root

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,8 +6,8 @@ generate_random_string() {
     cat /dev/urandom | tr -cd '[:alnum:]' | fold -w $length | head -n 1
 }
 
-prepare_sshd() {
-    SSHD_CUSTOM_CONFIG_FILE="/etc/ssh/sshd_config.d/99-custom-config.conf"
+configure_sshd() {
+    local SSHD_CUSTOM_CONFIG_FILE="/etc/ssh/sshd_config.d/99-custom-config.conf"
 
     # Default Config
     export SSHD_CONFIG_PasswordAuthentication=yes
@@ -21,7 +21,32 @@ prepare_sshd() {
     fi
 }
 
-prepare_user() {
+configure_root() {
+    echo ROOT_NAME="${ROOT_NAME:=root}"
+    echo ROOT_GROUP="${ROOT_GROUP:=root}"
+    echo ROOT_HOME_DIR="${ROOT_HOME_DIR:=/root}"
+    echo ROOT_PUBLIC_KEYS="$ROOT_PUBLIC_KEYS"
+    echo ROOT_PUBLIC_KEYS_URL="$ROOT_PUBLIC_KEYS_URL"
+
+    if [ -n "$ROOT_PUBLIC_KEYS" -o -n "$ROOT_PUBLIC_KEYS_URL" ]; then
+        local ROOT_SSH_DIR="$ROOT_HOME_DIR/.ssh"
+        local ROOT_AUTHORIZED_KEYS_FILE="$ROOT_SSH_DIR/authorized_keys"
+
+        mkdir "$ROOT_SSH_DIR"
+        chmod 700 "$ROOT_SSH_DIR"
+
+        if [ -n "$ROOT_PUBLIC_KEYS" ]; then
+            echo "$ROOT_PUBLIC_KEYS" > "$ROOT_AUTHORIZED_KEYS_FILE"
+        else
+            wget -O "$ROOT_AUTHORIZED_KEYS_FILE" "$ROOT_PUBLIC_KEYS_URL"
+        fi
+
+        chmod 600 "$ROOT_AUTHORIZED_KEYS_FILE"
+        chown -R "$ROOT_NAME:$ROOT_GROUP" "$ROOT_SSH_DIR"
+    fi
+}
+
+configure_user() {
     echo USER_NAME="${USER_NAME:=$(generate_random_string 16)}"
     echo USER_GROUP="${USER_GROUP:=$USER_NAME}"
     echo USER_PASSWORD="${USER_PASSWORD:=$(generate_random_string)}"
@@ -35,7 +60,7 @@ prepare_user() {
             -G "$USER_GROUP" \
             -D "$USER_NAME"
     
-    TEMP_CHPASSWD_PASSWORDS_FILE="$(mktemp)"
+    local TEMP_CHPASSWD_PASSWORDS_FILE="$(mktemp)"
     echo "$USER_NAME:$USER_PASSWORD" > "$TEMP_CHPASSWD_PASSWORDS_FILE"
     chpasswd < "$TEMP_CHPASSWD_PASSWORDS_FILE"
     rm -rf "$TEMP_CHPASSWD_PASSWORDS_FILE"
@@ -45,25 +70,27 @@ prepare_user() {
     fi
 
     if [ -n "$USER_PUBLIC_KEYS" -o -n "$USER_PUBLIC_KEYS_URL" ]; then
-        USER_SSH_DIR="$USER_HOME_DIR/.ssh"
-        AUTHORIZED_KEYS_FILE="$USER_SSH_DIR/authorized_keys"
+        local USER_SSH_DIR="$USER_HOME_DIR/.ssh"
+        local USER_AUTHORIZED_KEYS_FILE="$USER_SSH_DIR/authorized_keys"
 
         mkdir "$USER_SSH_DIR"
         chmod 700 "$USER_SSH_DIR"
 
         if [ -n "$USER_PUBLIC_KEYS" ]; then
-            echo "$USER_PUBLIC_KEYS" > "$AUTHORIZED_KEYS_FILE"
+            echo "$USER_PUBLIC_KEYS" > "$USER_AUTHORIZED_KEYS_FILE"
         else
-            wget -O "$USER_SSH_DIR/authorized_keys" "$USER_PUBLIC_KEYS_URL"
+            wget -O "$USER_AUTHORIZED_KEYS_FILE" "$USER_PUBLIC_KEYS_URL"
         fi
 
-        chmod 600 "$USER_SSH_DIR/authorized_keys"
+        chmod 600 "$USER_AUTHORIZED_KEYS_FILE"
         chown -R "$USER_NAME:$USER_GROUP" "$USER_SSH_DIR"
     fi
 }
 
-prepare_sshd
+configure_sshd
 
-prepare_user
+configure_root
+
+configure_user
 
 exec /usr/sbin/sshd -D
